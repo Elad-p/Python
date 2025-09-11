0341522fb1cc41a968144a498213fb912d57db29
Elad-p <49863249+Elad-p@users.noreply.github.com>
2025-09-11 10:36:00 +0300

    Update layout.py

--- streamlit_apps/layout/layout.py
+++ streamlit_apps/layout/layout.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-tips = pd.read_csv('tips.csv')
+tips = pd.read_csv('data/tips.csv')
 
 kpi1, kpi2, kpi3 = st.columns(3)
 
@@ -99,4 +99,5 @@ with t2:
                    title="Filtered: Total Bill vs Tip"),
         use_container_width=True
     )
+
 
