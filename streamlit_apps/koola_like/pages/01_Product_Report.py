ba83625fd43538899ad51ac8b14e9037aab1bd4f
Elad-p <49863249+Elad-p@users.noreply.github.com>
2026-01-25 18:20:31 +0200

    Update 01_Product_Report.py

--- streamlit_apps/koola_like/pages/01_Product_Report.py
+++ streamlit_apps/koola_like/pages/01_Product_Report.py
@@ -1,19 +1,3 @@
 import streamlit as st
 
-if 'counter' not in st.session_state:
-    st.session_state.counter = 0
-
-st.write('Current Value', st.session_state.counter)
-
-if st.button('Click me'):
-    st.session_state.counter += 1
-
-if st.button('Reset'):
-    st.session_state.counter = 0
-
-st.write('Counter Value: ', st.session_state.counter)
-
-x = {'a':1,
-     'b': 2}
-
-st.write(x.b)
+st.title('Product Report Page')
